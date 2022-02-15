7512761d7ec2934c7978805c17ce43023f998388
cpusch <42036285+cpusch@users.noreply.github.com>
2022-02-15 15:01:59 +0000

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': '2cb251721dmshf55a3f7bc6b90fdp1b5d06jsn4e14a9e4bc3d'
+    'x-rapidapi-key': 'key'
 }
 
 const baseURL = 'https://coinranking1.p.rapidapi.com';
